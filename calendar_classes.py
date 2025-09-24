6123dc399d254749b326dcea7412eaa4f98b36a1
williampeebles <wpeebles03@gardner-webb.edu>
2025-09-24 13:21:35 -0400

    Project creation

---
+++ calendar_classes.py
@@ -0,0 +1,30 @@
+"""
+- Let user choose what month it is
+- Specify what a whole month is
+- The object should include a list of events
+- Click on day then display a list of events
+- Buttons for calendar days
+- Start day, end Day
+
+"""
+import tkinter as tk
+import datetime
+class Calendar(object):
+    def __init__(self, month, day, year, dayOfWeek, events):
+        self.month = month
+        self.day = day
+        self.year = year
+        self.dayOfWeek = dayOfWeek
+        self.event = []
+
+
+class WeekGUI():
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.mainloop()
+
+
+
+
+
+WeekGUI()
